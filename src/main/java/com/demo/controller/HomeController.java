c0d7ab35e0ad33ec2a140bb9b8e83199f4576301
xxxzerone <enpunk90@gmail.com>
2024-10-07 19:28:56 +0900

    change homecontroller

--- src/main/java/com/demo/controller/HomeController.java
+++ src/main/java/com/demo/controller/HomeController.java
@@ -1,13 +1,16 @@
 package com.demo.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Slf4j
 @Controller
 public class HomeController {
 
     @GetMapping("/")
     public String index() {
+        log.info("HomeController index");
         return "index";
     }
 
